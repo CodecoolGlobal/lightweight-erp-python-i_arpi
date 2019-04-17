9c2a3407442454d4917d47923210abed4acbaac3
vnelli <varga.nelli9a@gmail.com>
2019-04-17 14:09:32 +0200

    hr almost working

--- hr/hr.py
+++ hr/hr.py
@@ -1,13 +1,3 @@
-""" Human resources module
-
-Data table structure:
-    * id (string): Unique and random generated identifier
-        at least 2 special characters (except: ';'), 2 number, 2 lower and 2 upper case letters)
-    * name (string)
-    * birth_year (number)
-"""
-
-# everything you'll need is imported:
 import sys
 import os
 # User interface module
@@ -19,19 +9,15 @@ import common
 
 
 def start_module():
-    """
-    Starts this module and displays its menu.
-     * User can access default special features from here.
-     * User can go back to main menu from here.
-
-    Returns:
-        None
-    """
-
-    table = data_manager.get_table_from_file("/home/vanelli/Documents/lightweight-erp-python-i_arpi/hr")
+    table = data_manager.get_table_from_file("/home/vanelli/Documents/lightweight-erp-python-i_arpi/hr/persons.csv")
     while True:
-        ui.print_show_menu_hr("HR menu", get_options, "Please enter a number:")
-    
+        ui.print_menu("HR menu", get_options(), "Go back to main menu")
+        try:
+            choose(table)
+        except KeyError as err:
+            ui.print_error_message(str(err))
+
+
 def choose(table):
     option = ui.get_inputs(["Please enter a number: "], "")
     if option[0] == "1":
@@ -56,7 +42,7 @@ def choose(table):
         ui.print_error_message("There is no such an option.")
 
 def get_options():
-    options = ["Display a table"
+    options = ["Display a table",
     "Add a new person",
     "Remove person",
     "Update infomation",
@@ -64,92 +50,34 @@ def get_options():
     "Who is the closest to the avarage age?"]
     return options
 
-
-def show_menu_hr():
-    ui.print_show_menu_hr("HR menu", options, "Enter a number: ")
+def get_label_list():
+    labels = ['ID', 'Name', 'Birth year']
+    return labels
 
 
 def show_table(table):
-    """
-    Display a table
-
-    Args:
-        table (list): list of lists to be displayed.
-
-    Returns:
-        None
-    """
-
-    # your code
     title_list = ['ID', 'Name', 'Birth date']
     ui.print_table(table, title_list)
 
 
 def add(table):
-    """
-    Asks user for input and adds it into the table.
-
-    Args:
-        table (list): table to add new record to
-
-    Returns:
-        list: Table with a new record
-    """
-
-    # your code
-
+    common.add(table, get_label_list(), 'Give new data:')
     return table
 
 
 def remove(table, id_):
-    """
-    Remove a record with a given id from the table.
-
-    Args:
-        table (list): table to remove a record from
-        id_ (str): id of a record to be removed
-
-    Returns:
-        list: Table without specified record.
-    """
-
-    # your code
 
     return table
 
 
 def update(table, id_):
-    """
-    Updates specified record in the table. Ask users for new data.
-
-    Args:
-        table (list): list in which record should be updated
-        id_ (str): id of a record to update
-
-    Returns:
-        list: table with updated record
-    """
-
-    # your code
-
+    
     return table
-
 
 # special functions:
 # ------------------
 
 def get_oldest_person(table):
-    """
-    Question: Who is the oldest person?
-
-    Args:
-        table (list): data table to work on
-
-    Returns:
-        list: A list of strings (name or names if there are two more with the same value)
-    """
-
-    # your code
     year = int(table[0][2])
     people_list = []
     for component in table:
@@ -163,17 +91,6 @@ def get_oldest_person(table):
 
 
 def get_persons_closest_to_average(table):
-    """
-    Question: Who is the closest to the average age?
-
-    Args:
-        table (list): data table to work on
-
-    Returns:
-        list: list of strings (name or names if there are two more with the same value)
-    """
-
-    # your code
     years = []
     for component in table:
         years.append(int(component[2]))
