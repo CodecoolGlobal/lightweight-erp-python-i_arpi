e1dfe6850e564cc77b3e1afc5ee7cbe3dd272f06
Daniel Kiraly <danielkiraly@ymail.com>
2019-04-30 10:58:44 +0200

    Implement menu function and last buyer's name function

--- data_analyser/data_analyser.py
+++ data_analyser/data_analyser.py
@@ -25,9 +25,47 @@ def start_module():
         None
     """
 
-    # your code
 
-    pass
+
+    while True:
+        ui.print_menu('Data Analyser module', get_options(), 'Back to main menu')
+        try:
+            if choose() == 'return':
+                return
+        except KeyError as err:
+            ui.print_error_message(str(err))
+
+
+def choose():
+    inputs = ui.get_inputs(["Please enter a number: "], "")
+    option = inputs[0]
+    if option == "1":
+        ui.print_result(get_the_last_buyer_name(), 'The name of the customer with thelast purchase is: ')
+    elif option == "2":
+        get_the_last_buyer_id()
+    elif option == "3":
+        get_the_buyer_name_spent_most_and_the_money_spent()
+    elif option == "4":
+        get_the_buyer_id_spent_most_and_the_money_spent()
+    elif option == "5":
+        get_the_most_frequent_buyers_names(num=1)
+    elif option == "6":
+        get_the_most_frequent_buyers_ids(num=1)
+    elif option == "0":
+        return 'return'
+    else:
+        raise KeyError("There is no such option.")
+
+
+def get_options():
+    options = ["Show the last buyer's name",
+               "Show the last buyer's ID",
+               "Show the buyer's name who spent most and the amount of money spent",
+               "Show the buyer's ID who spent most and the amount of money spent ",
+               "Show the most frequent's buyers' name",
+               "Show the most frequent's buyers' ID"]
+    return options
+
 
 
 def get_the_last_buyer_name():
@@ -38,7 +76,11 @@ def get_the_last_buyer_name():
         str: Customer name of the last buyer
     """
 
-    # your code
+    sale_id = sales.get_item_id_sold_last()
+    customer_id = sales.get_customer_id_by_sale_id(sale_id)
+    name = crm.get_name_by_id(customer_id)
+    return name
+
 
 
 def get_the_last_buyer_id():
